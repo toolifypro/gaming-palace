5c245c49b620b4badbc93edd5e875c673f154be2
toolifypro <yashrajsharma159@gmail.com>
2026-07-19 20:25:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyA34p9etp_BjR108MisCs0--C6ZSur3JWY",
+  apiKey:  "AIzaSyA34p9etp_BjR1O8MisCs0--C6ZSUr3JWY",
   authDomain: "gaming-beast-f73e3.firebaseapp.com",
   projectId: "gaming-beast-f73e3",
   storageBucket: "gaming-beast-f73e3.firebasestorage.app",
